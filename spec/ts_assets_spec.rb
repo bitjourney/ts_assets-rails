2929dc40891798452221c7e139bd6f583dd333fa
Kenju Wagatsuma <ken901waga@gmail.com>
2017-07-04 13:59:13 +0900

    git rm spec/build/assets.tsx and generate assets.tsx everytime for testing

--- spec/ts_assets_spec.rb
+++ spec/ts_assets_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe TsAssets do
   let(:include_path) { 'spec/assets/images' }
   
   describe '.generate' do
+    before do
+      system("bundle exec rake generate")
+    end
+
     it 'should generate a file content as a String' do
       source = TsAssets.generate(include: include_path)
       expect(source).to eq File.open("#{__dir__}/build/assets.tsx").read
